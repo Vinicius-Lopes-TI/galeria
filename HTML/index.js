b38ea4aea4c788e853e1fd06b3cdb63e91400d34
Vinícius Lopes <vinicius.lopes@mazzafc.tech>
2022-12-19 17:23:32 -0300

    Ajuste da galeria e inclusão de css.

--- HTML/index.js
+++ HTML/index.js
@@ -1,12 +1,9 @@
 // A $( document ).ready() block.
-$( document ).ready(function() {
-    var gallery = $('.gallery a').simpleLightbox({
-        
-            /* options */
-            sourceAttr:'href',
-            nav:true,
-
-        
-        });
-        
+$(document).ready(function () {
+  var gallery = $(".gallery a").simpleLightbox({
+    /* options */
+    sourceAttr: "href",
+    nav: true,
+    rel: false,
+  });
 });
